6b1e3f6b9c2276d1022d57711211f420b2858030
DarwinJS <DarwinJS@sanoys.com>
2020-06-22 07:34:18 -0400

    updates

--- runner_configs/linux-docker.sh
+++ runner_configs/linux-docker.sh
@@ -4,6 +4,21 @@
 
 # Update packages
 
+RunnerVersion="latest"
+RunnerExecutor='shell'
+RunnerOSTags="$($INSTANCEOSPLATFORM.ToLower())"
+RunnerTagList="TagA,TagB"
+RunnerConfigTomlTemplate #(Embedded, local or s3:// or http*://)
+GITLABRunnerRegTokenList='f3QN1vAeQq-MQx2_u9ML'
+RunnerGitLabInstanceURL='https://gitlab.demo.i2p.online/'
+RunnerInstallRoot='/gitlab-runner'
+RunnerConfigToml="$RunnerInstallRoot/config.toml"
+function logit() {
+  LOGSTRING="$(date +"%_b %e %H:%M:%S") $(hostname) USERDATA_SCRIPT: $1"
+  #For CloudFormation, if you already collect /var/log/cloud-init-output.log or /var/log/messsages (non amazon linux), then you could mute the next logging line
+  echo "$LOGSTRING" >> /var/log/messages
+}                     
+
 #Detect package manager
 if [[ -n "$(command -v yum)" ]] ; then
   PKGMGR='yum'
@@ -15,22 +30,20 @@ set -ex
 $PKGMGR update && $PKGMGR install -y wget
 
 # Installing and configuring Gitlab Runner
-wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
-chmod +x /usr/local/bin/gitlab-runner
+mkdir -p $RunnerInstallRoot
+wget kds -O $RunnerInstallRoot/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+chmod +x $RunnerInstallRoot/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 /usr/local/bin/gitlab-runner install --user="gitlab-runner" --working-directory="/home/gitlab-runner"
 echo -e "\nRunning scripts as '$(whoami)'\n\n"
 
-# Configuring Runner PreStart, Start and Stop Scripts
-cat > ~/start-gitlab-runner.sh << EOF
-/usr/local/bin/gitlab-runner run            \
+$RunnerInstallRoot/gitlab-runner run            \
   --working-directory "/home/gitlab-runner" \
   --config "/etc/gitlab-runner/config.toml" \
   --service "gitlab-runner"                 \
   --user "gitlab-runner"
-EOF
-cat > ~/register-gitlab-runner.sh << EOF
-/usr/local/bin/gitlab-runner register                          \
+
+$RunnerInstallRoot/gitlab-runner register                          \
   --non-interactive                                            \
   --url "https://<GITLAB_URL>/"                                \
   --registration-token "<GITLAB_REG_TOKEN"                     \
@@ -44,39 +57,36 @@ cat > ~/register-gitlab-runner.sh << EOF
   --docker-disable-cache false                                 \
   --docker-shm-size 0                                          \
   --locked="true"
-EOF
 
-cat > ~/deregister-gitlab-runner.sh << EOF
-/usr/local/bin/gitlab-runner "unregister" "--all-runners"
-EOF
+$RunnerInstallRoot/gitlab-runner unregister --all-runners
 
-echo -e "\nMaking scripts executable"
-chmod +x ~/{start,register,deregister}-gitlab-runner.sh
 
-# Move scripts to /usr/bin/
-mv ~/{start,register,deregister}-gitlab-runner.sh /usr/bin/
-echo -e "\nConfigure Gitlab Runner Service Unit File"
+#This approach for termination hook is much simpler than those involving SNS or CloudWatch, but when deployed 
+# on many instances it can result in a lot of ASG Describe API calls (which may be rate limited).
+ASGSelfMonitorTerminationInterval=${5ASGSelfMonitorTerminationInterval}
+if [ ! -z "$NAMEOFASG" ] && [ "$ASGSelfMonitorTerminationInterval" != "Disabled" ] && [ "$WaitingForReboot" != "true" ]; then
+  logit "Setting up termination monitoring because 5ASGSelfMonitorTerminationInterval is set to $ASGSelfMonitorTerminationInterval"
+  SCRIPTNAME=/etc/cron.d/MonitorTerminationHook.sh
+  SCRIPTFOLDER=$(dirname $SCRIPTNAME)
+  SCRIPTBASENAME=$(basename $SCRIPTNAME)
+  
+  #Heredoc script
+  cat << EndOfScript > $SCRIPTNAME
+    function logit() {
+      LOGSTRING="\$(date +"%_b %e %H:%M:%S") \$(hostname) TERMINATIONMON_SCRIPT: \$1"
+      echo "\$LOGSTRING"
+      echo "\$LOGSTRING" >> /var/log/messages
+    }
+    #These are resolved at script creation time to reduce api calls when this script runs every minute on instances.
 
-cat > ~/gitlab-runner.service << EOF
-[Unit]
-Description=GitLab Runner
-After=syslog.target network.target
-ConditionFileIsExecutable=/usr/local/bin/gitlab-runner
-[Service]
-StartLimitInterval=5
-StartLimitBurst=10
-ExecStart=/bin/bash /usr/bin/start-gitlab-runner.sh
-ExecStartPost=/bin/bash /usr/bin/register-gitlab-runner.sh
-ExecStop=/bin/bash /usr/bin/deregister-gitlab-runner.sh
-Restart=always
-RestartSec=120
-[Install]
-WantedBy=multi-user.target
-EOF
+    if [[ "\$(aws autoscaling describe-auto-scaling-instances --instance-ids $MYINSTANCEID --region $AWS_REGION | jq --raw-output '.AutoScalingInstances[0] .LifecycleState')" == *"Terminating"* ]]; then
+      logit "This instance ($MYINSTANCEID) is being terminated, perform cleanup..."
 
-# Move SystemD service unit file to /etc/systemd/system
-mv ~/gitlab-runner.service /etc/systemd/system/
-systemctl enable gitlab-runner
+      #### PUT YOUR CLEANUP CODE HERE, DECIDE IF CLEANUP CODE SHOULD ERROR OUT OR SILENTLY FAIL (best effort cleanup)
 
-# Service file changed - refresh systemd daemon
-systemctl daemon-reload
+      aws autoscaling complete-lifecycle-action --region $AWS_REGION --lifecycle-action-result CONTINUE --instance-id $MYINSTANCEID --lifecycle-hook-name instance-terminating --auto-scaling-group-name $NAMEOFASG
+      logit "This instance ($MYINSTANCEID) is ready for termination"
+      logit "Lifecycle CONTINUE was sent to termination hook in ASG: $NAMEOFASG for this instance ($MYINSTANCEID)."
+    fi
+
+EndOfScript
