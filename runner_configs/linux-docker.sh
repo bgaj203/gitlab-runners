272ec5a5f2f9afcaef19a6b882fc0a970886ac59
Darwin Sanoy <dsanoy@gitlab.com>
2020-11-12 16:00:31 -0500

    updates

--- runner_configs/linux-docker.sh
+++ runner_configs/linux-docker.sh
@@ -30,7 +30,7 @@ $PKGMGR update && $PKGMGR install -y wget
 
 # Installing and configuring Gitlab Runner
 mkdir -p $RunnerInstallRoot
-wget kds -O $RunnerInstallRoot/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+wget -O $RunnerInstallRoot/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
 chmod +x $RunnerInstallRoot/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
 $RunnerInstallRoot/gitlab-runner install --user="gitlab-runner" --working-directory="/home/gitlab-runner"
