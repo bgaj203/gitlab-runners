b3df932c61642ab27b3c4cdb517b6200718968ad
Darwin Sanoy <dsanoy@gitlab.com>
2021-02-11 20:46:49 -0500

    updates

--- runner_configs/windows-docker-windows.ps1
+++ runner_configs/windows-docker-windows.ps1
@@ -129,8 +129,7 @@ Start-Process msiexec.exe -Wait -ArgumentList "/i $env:public\amazon-cloudwatch-
 
 If (!(Test-Path $env:ProgramData\Amazon\AmazonCloudWatchAgent)) {New-Item $env:ProgramData\Amazon\AmazonCloudWatchAgent -ItemType Directory -Force}
 logit "Writing CloudWatch Agent configuration"
-set-content 
- -Value @'
+set-content set-content -Path "$env:ProgramData\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent.json" -Value @'
 {
   "metrics": {
     "aggregation_dimensions" : [["AutoScalingGroupName"], ["InstanceId"], ["InstanceType"], ["InstanceId","InstanceType"]],
